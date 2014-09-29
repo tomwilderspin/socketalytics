3b437da1709714d3d414f529917d5a964fc8c5e5
tomwilderspin <tomwilderspin@gmail.com>
2014-09-29 21:56:43 +0100

    first data send test

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 var server = require('http').Server(app);
-server.listen(3000, function (){
-    console.log('listening on *:3000');
+
+server.listen(3000, function(){
+    console.log('listen on *:3000');
 });
+
+var io = require('socket.io')(server);
+var capture = io.of('/capture');
+capture.on('connection', function(socket){
+    socket.on('client-data', function(data){
+        console.log(data);
+    });
+});
+
+io.on('connection', function(socket){
+    console.log('We have a connection');
+});
+
+
