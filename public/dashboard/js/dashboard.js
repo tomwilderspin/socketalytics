f99ea8d039706dec3cf21b2e62cb23640bd1c37e
tomwilderspin <tomwilderspin@gmail.com>
2014-09-29 22:38:16 +0100

    added charting

--- public/dashboard/js/dashboard.js
+++ public/dashboard/js/dashboard.js
@@ -1,3 +1,9 @@
 $( function() {
-  // Add code here
+  var visitors = $('#visitors').epoch({
+      type: 'time.area', axis:['left', 'bottom', 'right'],
+      data: [ { values: [{ time:Date.now()/1000, y:0 }] }],
+  });
+  var pages = $('#pages').epoch({type: 'bar'});
+  var touch = $('#touch').epoch({type: 'time.gauge'});
+  var video = $('#video').epoch({type: 'time.gauge'});
 } );
