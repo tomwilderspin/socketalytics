c7b8b5c068646c3858b801d7fe697bd9ff611b91
tomwilderspin <tomwilderspin@gmail.com>
2014-09-29 22:20:24 +0100

    added data store

--- index.js
+++ index.js
@@ -22,4 +22,24 @@ io.on('connection', function(socket){
     console.log('We have a connection');
 });
 
+var socketData = {};
+var stats = { connections: 0, touch:0, video:0, pages:{} };
+var capture = io.of('/capture');
+capture.on('connection', function(socket){
+    ++stats.connections;
+    socket.on('client-data', function(data){
+        socketData[ socket.id ] = data;
+        stats.touch += ( data.touch? 1 : 0 );
+        stats.video += ( data.video? 1 : 0 );
+        var pageCount = stats.pages[ data.url ] || 0;
+        stats.pages[ data.url ] = ++pageCount;
+    });
+});
+socket.on('disconnect', function(){
+    --stats.connections;
+    stats.touch -= ( socketData[ socket.id ].touch? 1:0 );
+    stats.video -= ( socketData[ socket.id ].video? 1:0 );
+    --stats.pages[ socketData[ sokcet.id ].url ];
+    delete socketData[ socket.id ];
+})
 
